f2da97dd107ae046d8ea21a2b18d2ddf0675f8f5
jim <303600370@qq.com>
2020-10-10 16:48:27 +0800

    md

--- mgr/run.go
+++ mgr/run.go
@@ -82,7 +82,7 @@ func (c *RunTask) run() {
 	for _, v := range c.plugs {
 		select {
 		case <-c.ctx.Done():
-			c.end(-1, "手动停止")
+			c.end(-1, "手动停止11")
 			return
 		default:
 			err := c.runs(v)
